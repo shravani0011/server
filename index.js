5b9e2eb35ccd116db3956cd854eac06a6c630092
Sravani maity <shravanimaity0011@gmail.com>
2026-02-27 18:39:29 +0530

    Fixed porting of server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();   
 
 const productRoutes = require("./routes/ProductRoutes");
 
@@ -11,14 +12,14 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-mongoose.connect(
-  "mongodb+srv://shravani27:maity123@cluster0.e9u7ml0.mongodb.net/products?retryWrites=true&w=majority"
-)
+const PORT = process.env.PORT || 5000;   
+
+mongoose.connect(process.env.MONGO_URI)  
 .then(() => {
   console.log("MongoDB Connected");
 
-  app.listen(5000, () => {
-    console.log("Server running on port 5000");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 })
 .catch(err => console.log(err));
